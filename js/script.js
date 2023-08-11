de4a99e35718ad940ac8c680f0588d591b981287
nesty11 <arielvale8@hotmail.com>
2023-08-11 17:00:44 -0400

    Started pokedex app script.js
    Added primary information for Pokedex app with first 9 pokemon, height and types.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-alert('Hello World!');
-
-let favoriteFood = 'Mofongo relleno de churrasco';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height:'2\'04"', type:["grass", "poison"]},
+    {name: 'Ivysaur', height: '3\'03"', type: ["grass", "poison"]},
+    {name: 'Venasaur', height: '6\'07"', type: ["grass", "poison"]},
+    {name: 'Charmander', height: '2\'0"', type: ["fire"]},
+    {name: 'Charmeleon', height: '3\'07"', type: ["fire"]},
+    {name: 'Charizard', height: '5\'07"', type: ["fire", "flying"]},
+    {name: 'Squirtle', height: '1\'08"', type: ["water"]},
+    {name: 'Wartortle', height: '3\'03"', type: ["water"]},
+    {name: 'Blastoise', height: '5\'03"', type: ["water"]}
+];
