ebdde79da8dff00ec7caadcb0234c19d45777514
nesty11 <arielvale8@hotmail.com>
2023-08-12 11:20:15 -0400

    Fixed height key
    Height key is now a value not string

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        height: '2',
+        height: 2,
         types: ["grass", "poison"]
     },
     {
         name: 'Ivysaur',
-        height: '3',
+        height: 3,
         types: ["grass", "poison"]
     },
     {
         name: 'Venasaur',
-        height: '6',
+        height: 6,
         types: ["grass", "poison"]
     },
     {
         name: 'Charmander',
-        height: '2',
+        height: 2,
         types: ["fire"]
     },
     {
         name: 'Charmeleon',
-        height: '3',
+        height: 3,
         types: ["fire"]
     },
     {
         name: 'Charizard',
-        height: '5',
+        height: 5,
         types: ["fire", "flying"]
     },
     {
         name: 'Squirtle',
-        height: '1',
+        height: 1,
         types: ["water"]
     },
     {
         name: 'Wartortle',
-        height: '3',
+        height: 3,
         types: ["water"]
     },
     {
         name: 'Blastoise',
-        height: '5',
+        height: 5,
         types: ["water"]
     }
 ];
